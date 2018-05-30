1888bff4ffca2d3947095dae74039b6485eef1f7
juporgar <julian.porgar@gmail,com>
2018-05-30 12:39:08 +0200

    instalación y creación de bower

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
+const hbsUtils = require("hbs-utils")(hbs);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//view engine partials
+hbsUtils.registerPartials(`${__dirname}/views/partials`);
+hbsUtils.registerWatchedPartials(`${__dirname}/views/partials`);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('./components',express.static(`${__dirname}/public/components`));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
