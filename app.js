fdf2fec6ab9481a49c3e684cb04ed8d134114e82
juporgar <julian.porgar@gmail,com>
2018-06-05 15:26:06 +0200

    falta el css y una parte del body

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const hbs = require('hbs');
-const hbsUtils = require("hbs-utils")(hbs);
+var hbs = require('hbs');
+var hbsUtils = require("hbs-utils")(hbs);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
+router.get('/quienessomos', function (req, res, next) { /* para hacer un llamamiento a quienessomos.hbs*/
+  res.render('quienessomos', {
+    title: 'Quienes Somos'
+  });
+});
 module.exports = router;
