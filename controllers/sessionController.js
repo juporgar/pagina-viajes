1fd611137211e91adfd41e6e9edad4b82336f8f2
unknown <julian.porgar@gmail.com>
2018-07-05 16:53:00 +0200

    Redirección al cerrar sesión

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -6,6 +6,7 @@ class sessionController extends Controller{
     }
     closeSession(){
         this.req.session.destroy(); // Para destruir las cookies y cerrar la sesion del usuario que estaba activado
+        this.res.render('/')
     }
 }
 module.exports = sessionController;
